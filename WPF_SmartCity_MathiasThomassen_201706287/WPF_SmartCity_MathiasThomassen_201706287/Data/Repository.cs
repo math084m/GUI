2bee590a4ab6eba89d3070c574d70c6a213b6a66
Mathias <math084m@hotmail.com>
2019-06-03 10:15:15 +0200

    layout halvvejs

---
+++ WPF_SmartCity_MathiasThomassen_201706287/WPF_SmartCity_MathiasThomassen_201706287/Data/Repository.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Serialization;
+
+//Class repository is made in lecture 11, to save agents to files.
+//This uses the same functionality, that the smartcity can use, and is therefore reused.
+
+namespace WPF_SmartCity_MathiasThomassen_201706287.Data
+{
+    class Repository
+    {
+        internal static bool ReadFile(string filename, out ObservableCollection<int> data)
+        {
+            XmlSerializer serializer = new XmlSerializer(typeof(ObservableCollection<int>));
+            TextReader reader = new StreamReader(filename);
+            data = (ObservableCollection<int>) serializer.Deserialize(reader);
+            reader.Close();
+            return true;
+        }
+
+        internal static void SaveFile(string filename, ObservableCollection<int> data)
+        {
+            XmlSerializer serializer = new XmlSerializer(typeof(ObservableCollection<int>));
+            TextWriter writer = new StreamWriter(filename);
+            serializer.Serialize(writer,data);
+            writer.Close();
+        }
+    }
+}
